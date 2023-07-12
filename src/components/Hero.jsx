16b45e6a5540611ae657cffcd3f5bf97082f2cc8
Bijay Sharma <89839141+bijay-gamd@users.noreply.github.com>
2023-07-12 09:53:07 +0530

    Add Content to Hero component and Analytics Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactTyped from "react-typed";
 
 const Hero = () => {
     return (
@@ -8,10 +9,14 @@ const Hero = () => {
                 <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
                     Grow with data.
                 </h1>
-<div className="flex justify-center items-center">
-    
-</div>
-
+                <div className="flex justify-center items-center">
+                    <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
+                        Fast, Flexible financing for
+                    </p>
+                    <ReactTyped  className="md:text-4xl sm:text-4xl text-xl font-bold md:pl-4 pl-2" strings={['BTB','BTC','SASS']}typeSpeed={120} backSpeed={140}loop/>
+                </div>
+                <p className="md:text-2xl text-xl font-bold text-gray-500">Monitor your data analytics to increase  revenue from BTB, BTC && SASS platforms</p>
+                <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">Get Started</button>
             </div>
         </div>
     );
