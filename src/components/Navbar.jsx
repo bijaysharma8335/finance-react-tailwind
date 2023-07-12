0d8d4ef4d5af6474225aa3b06b64003e50586a4b
Bijay Sharma <58084675+bijaysharma8335@users.noreply.github.com>
2023-07-12 10:53:23 +0530

    Add Content to Newsletter , Cards and Footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     };
     return (
         <div className="flex justify-between items-centerh-24 max-w-[1240px] mx-auto text-white">
-            <h1 className="w-full text-3xl font-bold text-[#36d93c]">REACT</h1>
+            <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT</h1>
             <ul className="hidden md:flex">
                 <li className="p-4">Home</li>
                 <li className="p-4">Company</li>
